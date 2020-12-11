d8a52cc9800243250f74b5729a067945f7c728aa
sky-admin <546485299@qq.com>
2020-12-11 15:24:38 +0800

    refactor: modify for getting version

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const program = require('commander')
 const chalk = require('chalk')
 const pkg = require('./package.json')
 const runParseCode = require('./lib/index')
+const { versionMap } = require('./lib/constant')
 
 const log = console.log
 
@@ -14,104 +15,20 @@ program
   .option('-a, --all', 'check code use all rules: include instance method & static method', false)
   .option('-e, --ecma <version>', 'check code use all rules: include instance method & static method', 6)
   .action((parseFiles, options) => {
-    let e
     const files = parseFiles.length ? parseFiles : []
+
     const esmodule = options.module
-    const version = options.ecma || 'es6'
+    const rawVersion = options.ecma || 'es6'
     const useAllRules = options.all
 
+    const version = versionMap[rawVersion]
+
     if (!version) {
-      log(chalk.red('No ecmaScript version'))
+      log(chalk.red('Invalid ecmaScript version'))
       process.exit(1)
     }
 
-    switch (version) {
-      case 'es3':
-        e = '3'
-        break
-      case 'es4':
-        e = '4'
-        break
-      case 'es5':
-        e = '5'
-        break
-      case 'es6':
-        e = '6'
-        break
-      case 'es7':
-        e = '7'
-        break
-      case 'es8':
-        e = '8'
-        break
-      case 'es9':
-        e = '9'
-        break
-      case 'es10':
-        e = '10'
-        break
-      case 'es11':
-        e = '11'
-        break
-      case 'es12':
-        e = '12'
-        break
-      case '3':
-        e = '3'
-        break
-      case '4':
-        e = '4'
-        break
-      case '5':
-        e = '5'
-        break
-      case '6':
-        e = '6'
-        break
-      case '7':
-        e = '7'
-        break
-      case '8':
-        e = '8'
-        break
-      case '9':
-        e = '9'
-        break
-      case '10':
-        e = '10'
-        break
-      case '11':
-        e = '11'
-        break
-      case '12':
-        e = '12'
-        break
-      case 'es2015':
-        e = '6'
-        break
-      case 'es2016':
-        e = '7'
-        break
-      case 'es2017':
-        e = '8'
-        break
-      case 'es2018':
-        e = '9'
-        break
-      case 'es2019':
-        e = '10'
-        break
-      case 'es2020':
-        e = '11'
-        break
-      case 'es2021':
-        e = '12'
-        break
-      default:
-        log(chalk.red('Invalid ecmaScript version'))
-        process.exit(1)
-    }
-    runParseCode(e, esmodule, files, useAllRules)
+    runParseCode(version, esmodule, files, useAllRules)
   })
 
 program.parse(process.argv)
